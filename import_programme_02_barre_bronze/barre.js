5f70f9b6ec71d108942f59ab65d634725efd40ce
debien1999 <christiandebienphotos01@gmail.com>
2025-03-30 19:05:03 -0400

    Ajout du fichier import_programme_02_barre_bronze/barre.js

---
+++ import_programme_02_barre_bronze/barre.js
@@ -0,0 +1,7 @@
+// Création de la barre bronze
+document.addEventListener("DOMContentLoaded", () => {
+  const taskbar = document.createElement("div");
+  taskbar.id = "taskbar";
+  taskbar.textContent = "Barre bronze prête !";
+  document.body.appendChild(taskbar);
+});
