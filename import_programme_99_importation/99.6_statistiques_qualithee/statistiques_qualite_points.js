6ad34fa88fc27abc4149759ac632985fa6149af2
debien1999 <christiandebienphotos01@gmail.com>
2025-03-30 20:12:13 -0400

    Ajout automatique de import_programme_99_importation/99.6_statistiques_qualithee/statistiques_qualite_points.js

--- import_programme_99_importation/99.6_statistiques_qualithee/statistiques_qualite_points.js
+++ import_programme_99_importation/99.6_statistiques_qualithee/statistiques_qualite_points.js
@@ -1,13 +1,1 @@
-// Suivi dynamique des points qualité par module
-const suiviQualite = [];
-
-function enregistrerQualite(module, score, commentaire) {
-  const point = {
-    module,
-    score,
-    commentaire,
-    horodatage: new Date().toISOString()
-  };
-  suiviQualite.push(point);
-  console.log('📊 Point qualité ajouté :', point);
-}
+// Suivi qualité...
